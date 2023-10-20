4af8c4d4b19b90136346d2daaea7097d91c2319f
edembl4m <mametov.edem@bk.ru>
2023-10-20 04:39:57 +0300

    basic logic

---
+++ scripts/card.js
@@ -0,0 +1,14 @@
+let cards = document.querySelectorAll('.catalog__card');
+let bottom = document.querySelectorAll('.bottom');
+
+
+for(let i = 0; i < cards.length; i++) {
+    cards[i].addEventListener('mouseover', function() {
+        this.classList.add('test');
+        bottom[i].classList.add('dop');
+    });
+    cards[i].addEventListener('mouseout', function(){
+        this.classList.remove('test');
+        bottom[i].classList.remove('dop');
+    });
+}
